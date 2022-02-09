005fcbb113bd98c8e067561d96a04fb109108937
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-02-09 23:47:48 +0530

    Adding the code for the pokemon displaying webpage

---
+++ pokemon sprite/index.js
@@ -0,0 +1,39 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+//code
+// const container = document.querySelector('#container');
+// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+// for (let i = 1; i <= 151; i++) {
+//    const pokemon = document.createElement('div');
+//    pokemon.classList.add('pokemon');
+//    const label = document.createElement('span');
+    
+//     label.innerText = `#${i}`;
+//     const newImg = document.createElement('img');
+//     newImg.src = `${baseURL}${i}.png`
+
+//     pokemon.appendChild(newImg);
+//     pokemon.appendChild(label);
+//     conatainer.appendChild(pokemon);
+// }
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const label = document.createElement('span');
+    
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon)
+}
