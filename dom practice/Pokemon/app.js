ef5b1dedfae5d0ac926a183c9633db9f248bdabb
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-03-16 20:24:09 +0530

    added a simple website that changes its background color on the click of a button

--- dom practice/Pokemon/app.js
+++ dom practice/Pokemon/app.js
@@ -1,24 +1,5 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-//code
-// const container = document.querySelector('#container');
-// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
-
-// for (let i = 1; i <= 151; i++) {
-//    const pokemon = document.createElement('div');
-//    pokemon.classList.add('pokemon');
-//    const label = document.createElement('span');
-    
-//     label.innerText = `#${i}`;
-//     const newImg = document.createElement('img');
-//     newImg.src = `${baseURL}${i}.png`
-
-//     pokemon.appendChild(newImg);
-//     pokemon.appendChild(label);
-//     conatainer.appendChild(pokemon);
-// }
-
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
@@ -27,7 +8,6 @@ for (let i = 1; i <= 151; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
     const label = document.createElement('span');
-
     label.innerText = `#${i}`;
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`
@@ -37,4 +17,3 @@ for (let i = 1; i <= 151; i++) {
     pokemon.appendChild(label);
     container.appendChild(pokemon)
 }
-
