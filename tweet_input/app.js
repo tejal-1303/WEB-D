918587728bbacb4ab7287779e121ad654a5998e8
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-03-19 23:37:55 +0530

    Tweet listing form added

---
+++ tweet_input/app.js
@@ -0,0 +1,7 @@
+const tweetForm = document.querySelector("#tweetForm");
+tweetForm.addEventListener("submit", function (e) {
+  const username = tweetForm.nextElementSibling.username.value;
+  const tweet = tweetForm.nextElementSibling.tweet.value;
+
+  const newTweet = document.createElemets;
+});
