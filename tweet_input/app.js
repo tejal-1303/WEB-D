48a8eb152d672c608a896c9102969f90f382a6dc
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-03-20 14:48:48 +0530

    Finsihed the tweet listing form

--- tweet_input/app.js
+++ tweet_input/app.js
@@ -1,7 +1,21 @@
 const tweetForm = document.querySelector("#tweetForm");
+const tweetsContainer = document.querySelector("#tweets");
 tweetForm.addEventListener("submit", function (e) {
-  const username = tweetForm.nextElementSibling.username.value;
-  const tweet = tweetForm.nextElementSibling.tweet.value;
+  e.preventDefault();
 
-  const newTweet = document.createElemets;
+  const usernameInput = tweetForm.elements.username;
+  const tweetInput = tweetForm.elements.tweet;
+
+  addTweet(usernameInput.value, tweetInput.value);
+  usernameInput.value = "";
+  tweetInput.value = "";
 });
+
+const addTweet = (username, tweet) => {
+  const newTweet = document.createElement("li");
+  const bTag = document.createElement("b");
+  bTag.append(username);
+  newTweet.append(bTag);
+  newTweet.append(` - ${tweet}`);
+  tweetsContainer.append(newTweet);
+};
